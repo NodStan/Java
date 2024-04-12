87ed692411afacacd77dde9f5f5ce6150d82bf9b
Stanley <stanleyobitunwase@gmail.com>
2024-04-12 13:21:51 +0100

    guessing game

---
+++ main.java
@@ -0,0 +1,12 @@
+package Area;
+
+import Hospital.Dog;
+
+public class main {
+    public static void main(String[] args) {
+        Square square = new Square(2);
+        Square square1 = new Square(2,5);
+        Square square2 = new Square(7.0);
+        Square square3 = new Square(7.0,5.0);
+    }
+}
